a43f64f7b5a4e6c5f0eb39e051f332abfc905f8e
Clarissa Fields <clarfields2016@gmail.com>
2023-04-28 20:36:19 -0400

    ProgramUI.cs: Train of thought comments added to help organize what may need to be done

--- REscapeRoom.UI/ProgramUI.cs
+++ REscapeRoom.UI/ProgramUI.cs
@@ -8,16 +8,18 @@ public class ProgramUI
     // public ProgramUI()
     // {
     //     SeedData();
-    //     _room = _hHouseRepo.GetEscapeRoom();
+    //     _room = _rEscapeRepo.GetEscapeRoom();
     // }
 
     public void Run()
     {
+        // Set the random number to generate the riddles and room hints (randNumber = ID assigned to the riddles/hints)
         RunApplication();
     }
 
     private void RunApplication()
     {
+
 
         while (IsRunning)
         {
@@ -59,16 +61,85 @@ public class ProgramUI
     private void StartGame()
     {
         Clear();
+        /*
+        ! This commented out section CAN BE MODIFIED
+        * Used to organize thoughts and train of thought on what is needed to be done
+        while (playerIsAlive == true && IsRunning == true)
+        {
+            System.Console.WriteLine("You have been trapped inside a building with three rooms. You need to solve a riddle for each room in order to escape. Try not to die! \n" +
+                                        "Press Any key to continue.");
+            ReadKey();
+
+            while (easyRiddleCorrect == false)
+            {
+                *LoadEasyRoom();
+            }
+
+            * Door opens and you move on to the next room 
+
+            while (mediumRiddleCorrect == false)
+            {
+                *LoadMediumRoom();
+            }
+
+            * Door opens and you move on to the final room 
+
+            while (hardRiddleCorrect == false)
+            {
+                *LoadHardRoom();
+            }
+
+            ? Could use an if statement as well, don't have to use switch
+            switch (playerIsAlive)
+                case true:
+                    *AliveGameEnd();
+                    break;
+                default:
+                    *DeadGameEnd();
+                    break;
+
+        }
+        */
         System.Console.WriteLine("Start Game");
         IsRunning = CloseGame(); // Line for testing purposes only, delete in real game
     }
 
     private void LoadEasyRoom()
-    { }
+    {
+        /*
+            TODO: will need to call upon the riddle/room entities using the random number to pull the information needed
+            * You enter the room and something speaks to you or you read the riddle by the door
+            * You can choose to answer the riddle right away or you can take a look around the room //? Get hints
+            * If you fail to answer the riddle in the correct ammount of time you die. //? Deaths are unique to the riddle/room you die in
+
+            * Convert user input to all lowercase //! No forgiveness for mispelled answers, and for now added spaces
+            * This will eliminate input errors of technically correct answers but are marked wrong due to caseSensitivity 
+            if (userAnsInput == RiddleAnswer)
+            {
+                easyRiddleCorrect == true;
+            }
+            else 
+            {
+                lose a life/tries remaining
+            }
+        */
+    }
     private void LoadMediumRoom()
-    { }
+    {
+        // Same process as the LoadEasyRoom()
+    }
     private void LoadHardRoom()
-    { }
+    {
+        // Same process as the LoadEasyRoom()
+    }
+    private void AliveGameEnd()
+    {
+        // YAY you escaped!
+    }
+    private void DeadGameEnd()
+    {
+        // BOO you died!
+    }
 
 
 
